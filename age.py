add32948d1430125786321a032028595e6c9ccab
Abhishek Maharjan <amaharjan08@gmail.com>
2019-07-12 00:40:14 -0700

    Age calculation script

---
+++ age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6
+
+name = input ("What is your name? ")
+birthday = input("Please enter your birthdate")
+age = int(input("How old are you?"))
+
+print(f"{name} was born in {birthday}")
+print(f"Half of your age is {age / 2}")
