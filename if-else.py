04f578b9cb424b42dcce4262d4c2626c40e8a006
Abhishek Maharjan <amaharjan08@gmail.com>
2019-07-11 22:41:59 -0700

    if-else example comit

---
+++ if-else.py
@@ -0,0 +1,10 @@
+user = {'admin': True, 'active': True, 'name': 'Abhishek'}
+prefix =""
+
+if user['admin'] and user['active']:
+    prefix = "ACTIVE - (ADMIN)"
+elif user['admin']:
+    prefix = "(ADMIN)"
+elif user['active']:
+    prefix = "ACTIVE -"
+print (f"prefix + user['name']")
